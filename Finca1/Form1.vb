fafbf4a374f8441ecad739404c98b0288c8dab9e
Gabriel FIerro <asistemas01@gvasti.com.mx>
2018-08-29 12:24:35 -0500

    Se agregó un mensaje de prueba

--- Finca1/Form1.vb
+++ Finca1/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            MsgBox("Mensaje de prueba")
+        Catch ex As Exception
 
+        End Try
+    End Sub
 End Class
